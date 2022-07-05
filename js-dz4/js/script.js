7aa54756daaab8b5532ce694ab714e3ef3217272
Salym7 <salym3k@gmail.com>
2022-07-05 18:00:25 +0300

    up

--- js-dz4/js/script.js
+++ js-dz4/js/script.js
@@ -1,13 +1,13 @@
 'use strict';
 
-let operator = prompt('Введите один из операторов (+ - * /) ')
-let num1 = +prompt('Введите первое число');
-let num2 = +prompt('Введите второе число');
 
-let add = num1 num2
-let sub = num1 - num2
-let mult = num1 * num2
-let div = num1 / num2
+const num1 = +prompt('Введите первое число');
+const num2 = +prompt('Введите второе число');
+
+const add = num1 + num2
+const sub = num1 - num2
+const mult = num1 * num2
+const div = num1 / num2
 
 console.log(`Результат: ${num1}+${num2}=${add}`)
 console.log(`Результат: ${num1}-${num2}=${sub}`)
