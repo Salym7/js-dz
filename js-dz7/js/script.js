c8e4cef029d6e6d1bc91c3753584677c50bb1b29
Salym7 <salym3k@gmail.com>
2022-07-10 23:15:29 +0300

    up dz-7

--- js-dz7/js/script.js
+++ js-dz7/js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
 const hours = +prompt('Введите количество часов ')
-const seconds = hours * 360
+const seconds = hours * 3600
 console.log(seconds)
 alert(`Количество секунд в ${hours} часов = ${seconds}`)
