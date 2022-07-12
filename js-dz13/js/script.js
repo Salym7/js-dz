89f2115d00a18249546dfb070d45e3d7a074c21b
Salym7 <salym3k@gmail.com>
2022-07-12 21:59:52 +0300

    dz-13

---
+++ js-dz13/js/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+// Task #1
+
+// let x = 10, y = 7;
+// x > y ? console.log('x больше, чем y') : console.log('x не больше, чем y');
+//
+// // Task #2
+//
+// const num = prompt('Введите число')
+// if (num % 2) {
+//     console.log('число ' + num + ' нечетное')
+// } else {
+//     console.log('число ' + num + ' четное')
+// }
+
+// Task #3
+
+// const num2 = prompt('Введите целое число')
+//
+// const long = num2.length
+// const sign = Math.sign(+num2)
+// let descrip
+//
+// if (sign === -1 || sign === -0) {
+//     descrip =  'Число ' + num2 + ' однозначно отрицательное' ;
+// } else if (sign === 1 || sign === 0) {
+//     descrip = 'Число ' + num2 + ' однозначно положительное';
+// } else {
+//     descrip = 'Число введено не правильно'
+// }
+//
+// alert('В числе ' + num2 + ' '+ num2.length + ' цифр ' + '\n' + descrip);
+
+// Task #4
+
+// const firstNumber = prompt('Введите первое число')
+// const secondNumber = prompt('Введите второе число')
+// const thirdNumber = prompt('Введите третье число')
+//
+// if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+//     alert('Первое число ' + firstNumber + ' самое большое число')
+// } else if (secondNumber > thirdNumber) {
+//     alert('Второе число ' + secondNumber + ' самое большое число')
+// } else {
+//     alert('Третье число ' + thirdNumber + ' самое большое число')
+// }
+
+// Task #5.
+
+// const firstSide = +prompt('Введите длину первой стороны')
+// const secondSide = +prompt('Введите длину второй стороны')
+// const thirdSide = +prompt('Введите длину третье стороны')
+//
+// if (firstSide < secondSide + thirdSide && thirdSide < secondSide + firstSide && secondSide < thirdSide + firstSide) {
+//     alert('Треугольник со сторонами: ' + ' a = ' + firstSide + ' b = ' + secondSide + ' c = ' + thirdSide + ' может существовать')
+// } else {
+//     alert('Треугольник со сторонами: ' + ' a = ' + firstSide + ' b = ' + secondSide + ' c = ' + thirdSide + ' не может существовать')
+// }
