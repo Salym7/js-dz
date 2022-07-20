0dde978310c493790709168b7b934d49da4213fd
Salym7 <salym3k@gmail.com>
2022-07-20 21:19:21 +0300

    dz-20

---
+++ js-dz20/js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const needSize = 48
+
+const onePrice = 13
+const oneWeight = 0.075
+
+function borsch(size) {
+    let weight = size * 4 * oneWeight
+    let price = onePrice * weight
+    return Math.ceil(price)
+}
+
+console.log('Что бы приготовить ' + needSize + ' литров борща, нужно купить картошки на ' + borsch(needSize) + 'грн')
+
