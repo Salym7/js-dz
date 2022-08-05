412c90243ee6be6dcf2d4e4deb611d08056e7000
Salym7 <salym3k@gmail.com>
2022-08-05 21:39:48 +0300

    dz-25

--- js-dz25/js/script.js
+++ js-dz25/js/script.js
@@ -1,84 +1,82 @@
 'use strict';
 
-// Task#1
-// const arr = [1, 2, 3, 4, 'less', 6, 7];
-//
-// const indexFunction = (find, array, start = 0) => {
-//     for (let i = start; i < array.length; i++) {
-//         if (array[i] === find) return i;
-//     }
-//     return -1;
-// }
-//
-// console.log(indexFunction('less', arr));
-// console.log(arr.indexOf('less'));
+const arr = [1, 2, 3, 4, 5, 6, 7];
 
-// Task#2
-// const arr = [1, 2, 3, 4, 5, 6, 7];
-//
-// const lastIndexFunction = (find, array, start = array.length - 1) => {
-//     for (let i = start; i >= 0; i--) {
-//         if (array[i] === find) return i;
-//     }
-//     return -1;
-// }
-//
-// console.log(indexFunction(6, arr));
-// console.log(arr.lastIndexOf(6));
-
-// Task#3
-// const arr = [1, 2, 3, 4, 5, 6, 7];
-// const findFunction = (array, current) => {
-//     for (let i = 0; i < array.length; i++) {
-//         if (array[i] === current) return array[i]
-//     }
-//
-//     return undefined
-// };
-// console.log(findFunction(arr, 2))
+const indexFunction = (array, find, start = 0) => {
+    for (let i = start; i < array.length; i += 1) {
+        if (array[i] === find) return i;
+    }
+    return -1;
+}
+console.log(indexFunction(arr, 5));
 
 
-// Task#4
-// const arr = [1, 2, 3, 4, 5, 6, 7];
-// const findIndexFunction = (array, current) => {
-//     for (let i = 0; i < array.length; i++) {
-//         if (array[i] === current) return i
-//     }
-//     return -1
-// };
-// console.log(findIndexFunction(arr, 5))
+const lastIndexFunction = (array, find, start) => {
+    if (!start) start = array.length - 1;
+    for (let i = start; i >= 0; i -= 1) {
+        if (array[i] === find) return i;
+    }
+    return -1;
+}
+console.log(lastIndexFunction(arr, 4,))
 
 
-// Task#5
-// const arr = [1, 2, 3, 4, 5, 6, 7];
-// const includesFunction = (array, current) => {
-//     for (let i = 0; i < array.length; i++) {
-//         if (array[i] === current) return true
-//     }
-//     return false
-// };
-// console.log(includesFunction(arr, 5))
+const findFunction = (array, callback) => {
+    for (let i = 0; i < array.length; i += 1) {
+        if (callback(array[i], i, array)) return array[i];
+    }
 
-// Task#6
-// const arr = [5, 6, 7, 8, 9, 10];
-// const everFunction = (array) => {
-//     for (let i = 0; i < array.length; i++) {
-//         if (array[i] > 4) continue
-//         return false
-//
-//     }
-//     return true
-// }
-// console.log(everFunction(arr))
+}
+console.log(
+    findFunction(arr, (item) => {
+        return item > 1;
+    })
+);
 
-// Task#7
-// const arr = [1, 3, 5, 7, 8];
-// const someFunction = (array) => {
-//
-//     for (let i = 0; i < array.length; i++) {
-//         if (array[i] % 2 === 0) return true
-//     }
-//     return false
-// }
-//
-// console.log(someFunction(arr))
+
+const findIndexFunction = (array, callback) => {
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) return array[i];
+    }
+    return -1;
+};
+console.log(
+    findIndexFunction(arr, (item) => {
+        return item > 3;
+    })
+);
+
+
+const includesFunction = (array, find, start = 0) => {
+    for (let i = start; i < array.length; i += 1) {
+        if (array[i] === find) return true;
+    }
+    return false;
+}
+console.log(includesFunction(arr, 10));
+
+
+const everyFunction = (array, callback) => {
+    for (let i = 0; i < array.length; i += 1) {
+        if (!callback(array[i], i, array)) return false;
+    }
+    return true;
+};
+console.log(
+    everyFunction(arr, (item) => {
+        return item > 2;
+    })
+);
+
+
+const someFunction = (array, callback) => {
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) return true;
+    }
+    return false;
+};
+console.log(
+    someFunction(arr, (item) => {
+        return item > 5;
+    })
+);
