d32013821be5df8040b8abefea79f7fa2875fb01
Oleksandr Solianyk <salym3k@gmail.com>
2022-08-21 19:04:44 +0300

    dz-29 fixed the bug

--- js-dz29/js/script.js
+++ js-dz29/js/script.js
@@ -1,21 +1,23 @@
 'use strict';
-
-const makeArray = () => {
+const makeArray = (max) => {
     let arr = []
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= max; i++) {
         arr.push(i)
     }
-    return () =>arr.splice(Math.floor(Math.random() * arr.length), 1)[0];
+    const getRandomNumber = () => arr.splice(Math.floor(Math.random() * arr.length), 1)[0];
+
+    return getRandomNumber
 }
 
-const random = makeArray()
+const random = makeArray(100)
+console.log(random());
+console.log(random());
+console.log(random());
+console.log(random());
 
-for (let i = 0; i < 100; i++) {
-    console.log(random());
-}
-
-
-
-
+// for (let i = 0; i < 100; i++) {
+//     console.log(random());
+// }
+//
 
 
