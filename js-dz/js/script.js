f01320b6b90a9df06c958e4f4e400e574dedcb7b
Oleksandr Solianyk <salym3k@gmail.com>
2022-08-29 15:17:08 +0300

    classwork

--- js-dz/js/script.js
+++ js-dz/js/script.js
@@ -1,5 +1,27 @@
 'use strict';
 
+// const mono = (str) => {
+//     const obj = {}
+//     for (let l of str.toLowerCase()) {
+//         obj[l] = obj[l] + 1 || 1
+//     }
+//     return obj
+// }
+//
+// const anagramma = (str1, str2) => {
+//     const obj1 = mono(str1)
+//     const obj2 = mono(str2)
+//     console.log(Object.keys(obj1));
+//     if (Object.keys(obj1).length !== Object.keys(obj2).length) return false
+//     for (let l in obj1) {
+//         if (obj1[l] !== obj2[l]) return false
+//     }
+//     return true
+// }
+//
+// console.log(anagramma('Привет', 'тевирП'));
+
+
 // const bar = (str) => {
 //     if (typeof str !== 'string') return false
 //     const fil = str.split('.')
