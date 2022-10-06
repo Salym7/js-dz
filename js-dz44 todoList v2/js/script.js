0a56de814151b72f85bc8f6337509f637b7e0667
Oleksandr Solianyk <salym3k@gmail.com>
2022-10-06 22:53:01 +0300

    todoList v2

--- js-dz44 todoList v2/js/script.js
+++ js-dz44 todoList v2/js/script.js
@@ -11,9 +11,10 @@ const toDoList = {
         if (typeof selector === "string" || selector.trim() !== '') this.selector = selector;
         if (typeof container === 'string' || container.trim() !== '') this.containerSelector = container;
 
-        this.getId()
+        this.getId();
         this.getForm();
-        this.getHTMLElement()
+        this.getHTMLElement();
+        this.removeItems();
     },
     getId() {
         if (localStorage.getItem(this.selector) && JSON.parse(localStorage.getItem(this.selector)).length) {
@@ -33,6 +34,7 @@ const toDoList = {
                 .forEach((item) => {
                     data[item.name] = item.value;
                 })
+            data.id = this.id += 1;
             const savedData = this.saveData(data);
             this.renderItem(savedData);
         })
@@ -67,99 +69,37 @@ const toDoList = {
     },
 
     renderItem(data) {
-        this.id += 1
+        const id = data.id
         const title = data.title;
         const description = data.description;
         const wrapper = document.createElement('div');
-        wrapper.setAttribute('data-id', this.id);
+        wrapper.setAttribute('data-id', id);
         wrapper.classList.add('col-4');
         wrapper.innerHTML = `<div class="taskWrapper">
                             <div class="taskHeading">${title}</div>
                             <div class="taskDescription">${description}</div>
                          </div>`;
         this.container.appendChild(wrapper);
-    }
+    },
 
+    removeItems() {
+        this.container.addEventListener('click', e => {
+            e.stopPropagation();
+
+            const currentItem = e.target.closest('[data-id]');
+            const currentItemId = Number(currentItem.getAttribute('data-id'));
+            console.log(currentItem);
+
+            const filteredData = JSON
+                .parse(localStorage.getItem(this.selector))
+                .filter(item => item.id !== currentItemId);
+
+            console.log(filteredData);
+            localStorage.setItem(this.selector, JSON.stringify(filteredData));
+            currentItem.remove();
+        })
+    }
 }
 toDoList.init('#todoForm', '#todoItems');
 
-// (function () {
-//
-//     const dataKey = 'formData';
-//     let id = 0;
-//     const form = document.querySelector('#todoForm');
-//     const todoItems = document.querySelector('#todoItems');
-//
-//     if (localStorage.getItem(dataKey) && JSON.parse(localStorage.getItem(dataKey)).length) {
-//         const data = JSON.parse(localStorage.getItem(dataKey));
-//         id = data[data.length - 1].id;
-//     }
-//
-//     const renderItem = (todoItem) => {
-//         const wrapper = document.createElement('div');
-//         wrapper.classList.add('col-4');
-//         wrapper.setAttribute('data-id', todoItem.id);
-//
-//        wrapper.innerHTML = `
-//                <div class="taskWrapper" >
-//                    <div class="taskHeading">${todoItem.title}</div>
-//                    <div class="taskDescription">${todoItem.description}</div>
-//                </div>`
-//
-//         todoItems.prepend(wrapper);
-//     };
-//
-//
-//     form.addEventListener('submit', event => {
-//         event.preventDefault();
-//         event.stopPropagation();
-//
-//         const {target} = event;
-//
-//         let data = target.querySelectorAll('input,textarea');
-//
-//         data = Array.from(data)
-//             .reduce((acc, item) => {
-//                 acc[item.name] = item.value;
-//                 return acc;
-//             }, {})
-//
-//         data.id = id += 1;
-//
-//         const dataToSave =
-//             localStorage.getItem(dataKey) ?
-//                 JSON.parse(localStorage.getItem(dataKey)) :
-//                 [];
-//         dataToSave.push(data);
-//
-//         localStorage.setItem(dataKey, JSON.stringify(dataToSave));
-//
-//         renderItem(data);
-//
-//     })
-//
-//     document.addEventListener('DOMContentLoaded', () => {
-//         if (!localStorage.getItem(dataKey)) return;
-//
-//         const data = JSON.parse(localStorage.getItem(dataKey))
-//         data.forEach(item => renderItem(item))
-//     })
-//
-//     todoItems.addEventListener('click', e => {
-//         e.stopPropagation()
-//
-//         const currentItem = e.target.closest('[data-id]');
-//         const currentItemId = Number(currentItem.getAttribute('data-id'));
-//         console.log(currentItem);
-//
-//         const filteredData = JSON
-//             .parse(localStorage.getItem(dataKey))
-//             .filter(item => item.id !== currentItemId);
-//
-//         console.log(filteredData);
-//         localStorage.setItem(dataKey, JSON.stringify(filteredData));
-//         currentItem.remove();
-//     })
-//
-// }())
 
