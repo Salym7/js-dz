eb36031ba2cb06eae0bc9341af9d6105a30ea663
Oleksandr Solianyk <salym3k@gmail.com>
2022-11-03 22:22:48 +0200

    init

--- cache/js/script.js
+++ cache/js/script.js
@@ -1,5 +1,42 @@
 'use strict';
 
 (() => {
+    const cache = new Map();
+
+    function cacheFn(funcCallback) {
+        if (cache.has(funcCallback)) {
+            cache.set(funcCallback, funcCallback());
+            return funcCallback
+        }
+        cache.set(funcCallback, funcCallback());
+        return funcCallback
+    }
+
+    function sum(a, b) {
+        return function sum1 (a, b){
+           return  a + b;
+        }
+
+    }
+
+    cacheFn(sum(1, 2))
+    cacheFn(sum(2, 2))
+    cacheFn(sum(3, 3))
+    cacheFn(sum(4, 2))
+    cacheFn(sum(3, 2))
+
+    // cacheFn(sum(4, 2))
+    // cacheFn(sum(4, 3))
+    // cacheFn(sum(4, 5))
+    // cacheFn(sum(4, 6))
+    // cacheFn(sum(4, 7))
+    // cacheFn(sum(4, 8))
+    // cacheFn(sum(0, 7))
+    // cacheFn(sum(1, 8))
+
+    console.log(cache);
+    console.log(cacheFn(sum(4, 2)));
+
 
 })();
+
