c0d50bf14c418344504c23f7971c987f28f75dca
Oleksandr Solianyk <salym3k@gmail.com>
2022-12-05 18:03:11 +0200

    solution

--- fetch gallery/js/script.js
+++ fetch gallery/js/script.js
@@ -1,103 +1,80 @@
 'use strict';
+(function () {
 
-const toDoList = {
-    selector: null,
-    form: null,
-    containerSelector: null,
-    container: null,
-    id: 0,
+    const album = document.querySelector('#album');
+    const photo = document.querySelector('#photo');
 
-    init(selector, container) {
-        if (typeof selector === "string" || selector.trim() !== '') this.selector = selector;
-        if (typeof container === 'string' || container.trim() !== '') this.containerSelector = container;
-
-        this.getId();
-        this.getForm();
-        this.getHTMLElement();
-        this.removeItems();
-    },
-    getId() {
-        if (localStorage.getItem(this.selector) && JSON.parse(localStorage.getItem(this.selector)).length) {
-            const data = JSON.parse(localStorage.getItem(this.selector));
-            this.id = data[data.length - 1].id;
-        }
-    },
-    getForm() {
-        const formElement = document.querySelector(this.selector);
-        this.form = formElement;
-        formElement.addEventListener('submit', event => {
-            event.preventDefault();
-            event.stopPropagation();
-            const data = {};
-            formElement
-                .querySelectorAll('input, textarea')
-                .forEach((item) => {
-                    data[item.name] = item.value;
-                })
-            data.id = this.id += 1;
-            const savedData = this.saveData(data);
-            this.renderItem(savedData);
-        })
-    },
-
-    getHTMLElement() {
-        this.container = document.querySelector(this.containerSelector);
-        document.addEventListener('DOMContentLoaded', event => {
-            event.preventDefault();
-            event.stopPropagation();
-            const toDo = JSON.parse(localStorage.getItem(this.selector));
-            if (!toDo) return 'localStorage is empty';
-            toDo.map(todoItem => {
-                this.renderItem(todoItem);
+    fetch('https://jsonplaceholder.typicode.com/albums')
+        .then(resolve => resolve.json())
+        .then(data => {
+            console.log(data)
+            data.forEach(item => {
+                renderAlbum(item)
             })
         })
-    },
+        .then((data) => {
+            album.addEventListener('click', e => {
+                e.stopPropagation();
+                photo.replaceChildren()
+                console.log(e.target.closest('[data-id]'));
 
-    saveData(data) {
-        let dataFromStore = localStorage.getItem(this.selector);
-        if (!dataFromStore) {
-            const array = [];
-            array.push(data);
-            localStorage.setItem(this.selector, JSON.stringify(array));
-        }
-        if (dataFromStore) {
-            dataFromStore = JSON.parse(dataFromStore);
-            dataFromStore.push(data);
-            localStorage.setItem(this.selector, JSON.stringify(dataFromStore));
-        }
-        return data;
-    },
+                const currentItem = e.target.closest('[data-id]')
 
-    renderItem(data) {
-        const id = data.id
+                const currentItemId = currentItem.getAttribute('data-id')
+                const url = `https://jsonplaceholder.typicode.com/photos?albumId=${currentItemId}`
+                getAlbum(url)
+                return currentItemId
+            })
+        })
+
+    const getAlbum = (url) => {
+        fetch(url)
+            .then(resolve => resolve.json())
+            .then(data => {
+                console.log(data)
+                data.forEach(item => {
+                    renderPhoto(item)
+                })
+            })
+    }
+
+    const renderAlbum = (data) => {
+        const userId = data.userId
+        const id = data.id;
         const title = data.title;
-        const description = data.description;
+
         const wrapper = document.createElement('div');
         wrapper.setAttribute('data-id', id);
-        wrapper.classList.add('col-4');
+        wrapper.classList.add('box');
         wrapper.innerHTML = `<div class="taskWrapper">
-                            <div class="taskHeading">${title}</div>
-                            <div class="taskDescription">${description}</div>
+                            <div class="taskHeading">User ${userId}</div>
+                            <div class="taskDescription">${title}</div>
                          </div>`;
-        this.container.appendChild(wrapper);
-    },
-
-    removeItems() {
-        this.container.addEventListener('click', e => {
-            e.stopPropagation();
-
-            const currentItem = e.target.closest('[data-id]');
-            const currentItemId = Number(currentItem.getAttribute('data-id'));
-
-            const filteredData = JSON
-                .parse(localStorage.getItem(this.selector))
-                .filter(item => item.id !== currentItemId);
-
-            localStorage.setItem(this.selector, JSON.stringify(filteredData));
-            currentItem.remove();
-        })
+        album.appendChild(wrapper);
     }
-}
-toDoList.init('#todoForm', '#todoItems');
+
+    const renderPhoto = data => {
+        const albumId = data.userId
+        const id = data.id;
+        const title = data.title;
+        const url = data.url;
+        const thumbnailUrl = data.thumbnailUrl
+
+        const wrapper = document.createElement('div');
+        wrapper.setAttribute('data-id', id);
+        wrapper.classList.add('box');
+        wrapper.innerHTML = `<div class="taskWrapper">
+                                <p class="albumId">User ${albumId}</p>
+                                <p class="id">User ${id}</p>
+                                <p class="title">User ${title}</p>
+                                <img src="${url}" alt="img ${id}">
+                                <img src="${thumbnailUrl}" alt="img ${id}">
+                         </div>`;
+
+        photo.appendChild(wrapper);
+
+    }
+
+}())
 
 
