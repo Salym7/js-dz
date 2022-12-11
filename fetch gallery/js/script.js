9a20efd3bcbbff994c88524336058f881464a16b
Oleksandr Solianyk <salym3k@gmail.com>
2022-12-11 12:30:27 +0200

    solution by Class

--- fetch gallery/js/script.js
+++ fetch gallery/js/script.js
@@ -1,50 +1,108 @@
 'use strict';
 (function () {
 
-    const album = document.querySelector('#album');
-    const photo = document.querySelector('#photo');
+    class Gallery {
+        album = null;
+        // photo = null;
+        url = null;
 
-    fetch('https://jsonplaceholder.typicode.com/albums')
-        .then(resolve => resolve.json())
-        .then(data => {
-            console.log(data)
-            data.forEach(item => {
-                renderAlbum(item)
-            })
-        })
-        .then((data) => {
-            album.addEventListener('click', e => {
-                e.stopPropagation();
-                // photo.replaceChildren()
-                console.log(e.target.closest('[data-id]'));
-                //
-                const currentItem = e.target.closest('[data-id]')
+        constructor(album,url) {
+            if (typeof album === "string" || album.trim() !== '') this.album = document.querySelector(album);
+            // if (typeof photo === 'string' || photo.trim() !== '') this.photo = document.querySelector(photo);
+            if (typeof url === 'string' || url.trim() !== '') this.url = url;
+            this.getData(this.url)
+            console.log(this.album);
+        }
 
-                const currentItemId = currentItem.getAttribute('data-id')
-                localStorage.clear()
-                localStorage.setItem('id', currentItemId)
+        getData(url) {
+            fetch(url)
+                .then(resolve => resolve.json())
+                .then(data => {
+                    this.data = data
+                    console.log(data)
+                    data.forEach(item => {
+                        this.renderAlbum(item)
+                    })
+                })
+                .then((data) => {
+                    this.album.addEventListener('click', e => {
+                        e.stopPropagation();
+                        // photo.replaceChildren()
+                        console.log(e.target.closest('[data-id]'));
 
-                // window.open('./photo.html')
+                        const currentItem = e.target.closest('[data-id]')
 
-                return currentItemId
-            })
-        })
+                        const currentItemId = currentItem.getAttribute('data-id')
+                        localStorage.clear()
+                        localStorage.setItem('id', currentItemId)
+                        return currentItemId
+                    })
+                })
+        }
 
 
-    const renderAlbum = (data) => {
-        const userId = data.userId
-        const id = data.id;
-        const title = data.title;
+        renderAlbum(data){
+            const userId = data.userId
+            const id = data.id;
+            const title = data.title;
 
-        const wrapper = document.createElement('div');
-        wrapper.setAttribute('data-id', id);
-        wrapper.classList.add('box', 'box-album');
-        wrapper.innerHTML = `<a class="taskWrapper" href="./photo.html">
+            const wrapper = document.createElement('div');
+            wrapper.setAttribute('data-id', id);
+            wrapper.classList.add('box', 'box-album');
+            wrapper.innerHTML = `<a class="taskWrapper" href="./photo.html">
                             <div class="taskHeading">User ${userId}</div>
                             <div class="taskDescription">${title}</div>
                          </a>`;
-        album.appendChild(wrapper);
+            this.album.appendChild(wrapper);
+        }
+
+
     }
+
+    new Gallery('#album','https://jsonplaceholder.typicode.com/albums');
+
+    // const album = document.querySelector('#album');
+    // const photo = document.querySelector('#photo');
+    //
+    // fetch('https://jsonplaceholder.typicode.com/albums')
+    //     .then(resolve => resolve.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         data.forEach(item => {
+    //             renderAlbum(item)
+    //         })
+    //     })
+    //     .then((data) => {
+    //         album.addEventListener('click', e => {
+    //             e.stopPropagation();
+    //             // photo.replaceChildren()
+    //             console.log(e.target.closest('[data-id]'));
+    //             //
+    //             const currentItem = e.target.closest('[data-id]')
+    //
+    //             const currentItemId = currentItem.getAttribute('data-id')
+    //             localStorage.clear()
+    //             localStorage.setItem('id', currentItemId)
+    //
+    //             return currentItemId
+    //         })
+    //     })
+    //
+    //
+    // const renderAlbum = (data) => {
+    //     const userId = data.userId
+    //     const id = data.id;
+    //     const title = data.title;
+    //
+    //     const wrapper = document.createElement('div');
+    //     wrapper.setAttribute('data-id', id);
+    //     wrapper.classList.add('box', 'box-album');
+    //     wrapper.innerHTML = `<a class="taskWrapper" href="./photo.html">
+    //                         <div class="taskHeading">User ${userId}</div>
+    //                         <div class="taskDescription">${title}</div>
+    //                      </a>`;
+    //     album.appendChild(wrapper);
+    // }
 
 
 }())
