61573ecf5306c8087f4b2f58dfa857e19da85c89
Oleksandr Solianyk <salym3k@gmail.com>
2022-12-11 12:30:07 +0200

    solution by Class

--- fetch gallery/js/script_photo.js
+++ fetch gallery/js/script_photo.js
@@ -2,40 +2,47 @@
 
 
 (function () {
+    class GalleryPhoto {
 
+        photo = null;
 
-    document.addEventListener('DOMContentLoaded', event => {
-        event.preventDefault();
-        event.stopPropagation();
-        const id = JSON.parse(localStorage.getItem('id'));
+        constructor(photo, url) {
+            if (typeof photo === 'string' || photo.trim() !== '') this.photo = document.querySelector(photo);
+            this.getUrl(url)
+        }
 
-        const url = `https://jsonplaceholder.typicode.com/photos?albumId=${id}`
-        getAlbum(url)
-        return id
-    })
-
-    const getAlbum = (url) => {
-        fetch(url)
-            .then(resolve => resolve.json())
-            .then(data => {
-                console.log(data);
-                data.forEach(item => {
-                    renderPhoto(item);
-                })
+        getUrl() {
+            document.addEventListener('DOMContentLoaded',  (event) => {
+                event.preventDefault();
+                event.stopPropagation();
+                const id = JSON.parse(localStorage.getItem('id'));
+                const url = `https://jsonplaceholder.typicode.com/photos?albumId=${id}`
+                this.getAlbum(url)
             })
-    }
+        }
 
-    const renderPhoto = data => {
-        const albumId = data.userId
-        const id = data.id;
-        const title = data.title;
-        const url = data.url;
-        const thumbnailUrl = data.thumbnailUrl
+        getAlbum(url) {
+            fetch(url)
+                .then(resolve => resolve.json())
+                .then(data => {
+                    console.log(data);
+                    data.forEach(item => {
+                        this.renderPhoto(item);
+                    })
+                })
+        }
 
-        const wrapper = document.createElement('div');
-        wrapper.setAttribute('data-id', id);
-        wrapper.classList.add('box');
-        wrapper.innerHTML = `<div class="taskWrapper">
+        renderPhoto(data) {
+            const albumId = data.userId
+            const id = data.id;
+            const title = data.title;
+            const url = data.url;
+            const thumbnailUrl = data.thumbnailUrl
+
+            const wrapper = document.createElement('div');
+            wrapper.setAttribute('data-id', id);
+            wrapper.classList.add('box');
+            wrapper.innerHTML = `<div class="taskWrapper">
                                 <p class="albumId">User ${albumId}</p>
                                 <p class="id">User ${id}</p>
                                 <p class="title">User ${title}</p>
@@ -43,8 +50,11 @@
                                 <img src="${thumbnailUrl}" alt="img ${id}">
                          </div>`;
 
-        photo.appendChild(wrapper);
+            this.photo.appendChild(wrapper);
+
+        }
 
     }
 
+    new GalleryPhoto('#photo', 'https://jsonplaceholder.typicode.com/photos?albumId=')
 }())
