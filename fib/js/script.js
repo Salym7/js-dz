bfb1504fe84b62c8ad468e86792dd07119f7538a
Oleksandr Solianyk <salym3k@gmail.com>
2023-01-08 18:21:00 +0200

    add solution fib

---
+++ fib/js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const fib = (x = 1) => {
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= x; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+console.log(fib(5));
